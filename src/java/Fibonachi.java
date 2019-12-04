8bd06dc80a8fcb55dee5e6a14f7820c9e27fd0e9
Olgatltt <olgatltt@rambler.ru>
2019-12-04 15:07:11 +0400

    Добавила класс Factorial и Fibonachi

---
+++ src/java/Fibonachi.java
@@ -0,0 +1,11 @@
+public class Fibonachi {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i < 10; i++) {
+            b = a + b;
+            a = b - a;
+            System.out.println(b);
+        }
+    }
+}
